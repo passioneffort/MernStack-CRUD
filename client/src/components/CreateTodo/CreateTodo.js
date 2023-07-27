3831a50b60d8639f89d277e993479672a66dd15d
passioneffort <agronkercishta.ai@gmail.com>
2023-07-27 12:01:45 -0400

    modify

--- client/src/components/CreateTodo/CreateTodo.js
+++ client/src/components/CreateTodo/CreateTodo.js
@@ -1,28 +1,28 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import axios from "axios";
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
 
-const CreateTodo = () => {
-  const [todoInfo, setTodoInfo] = useState({ title: "", description: "" });
+// const CreateTodo = () => {
+//   const [todoInfo, setTodoInfo] = useState({ title: "", description: "" });
 
-  function handleChange(e) {
-    setTodoInfo((data) => ({ ...data, [e.target.name]: e.target.value }));
-  }
+//   function handleChange(e) {
+//     setTodoInfo((data) => ({ ...data, [e.target.name]: e.target.value }));
+//   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+//   function handleSubmit(e) {
+//     e.preventDefault();
 
-    axios
-      .post("http://localhost:5000/api/todoapp", todoInfo)
-      .then((res) => {
-        setTodoInfo({ title: "", description: "" });
-        console.log(res.data.message);
-      })
-      .catch((err) => {
-        console.log("Error couldn't create TODO");
-        console.log(err.message);
-      });
-  }
+//     axios
+//       .post("http://localhost:5000/api/todoapp", todoInfo)
+//       .then((res) => {
+//         setTodoInfo({ title: "", description: "" });
+//         console.log(res.data.message);
+//       })
+//       .catch((err) => {
+//         console.log("Error couldn't create TODO");
+//         console.log(err.message);
+//       });
+//   }
 
   return (
     <section className="container">
