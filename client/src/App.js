499e8e61bfce42caaf85b066ba47526873de5d30
passioneffort <agronkercishta.ai@gmail.com>
2023-07-30 13:33:25 -0400

    modify

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import DisplayTodo from './components/DisplayTodo';
-// import CreateTodo from './components/CreateTodo';
-// import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import DisplayTodo from './components/DisplayTodo';
+import CreateTodo from './components/CreateTodo';
+import './App.css';
 
-// function App() {
-//   return (
-//     <div className="todo-Container">
-//       <BrowserRouter>
-//         <Route exact path="/" component={DisplayTodo} />
-//         <Route path="/add-list" component={CreateTodo} />
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="todo-Container">
+      <BrowserRouter>
+        <Route exact path="/" component={DisplayTodo} />
+        <Route path="/add-list" component={CreateTodo} />
+      </BrowserRouter>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
